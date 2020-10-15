4f19aec7ee506333aca6bb044ba2ee316d4dcfaf
olivierdg2 <16027@ecam.be>
2020-10-15 11:57:52 +0200

    Update reader.go

--- exercises/exo2/reader.go
+++ exercises/exo2/reader.go
@@ -2,20 +2,30 @@ package main
 
 import (
 	"io"
-	"os"
-	"strings"
+    "strings"
+    "os"
+    "bytes"
 )
 
 type spaceEraser struct {
 	r io.Reader
 }
 
-func main() {
-	s := strings.NewReader("H e l l o w o r l d!")
-	r := spaceEraser{s}
-	io.Copy(os.Stdout, &r)
-}
 
-// Implement a type that satisfies the io.Reader interface and reads from another io.Reader,
-// modifying the stream by removing the spaces.
-// Expected output: "Helloworld!"
+func (s spaceEraser) Read(p []byte) (int, error) {
+    n, err := s.r.Read(p)
+    //Remove all spaces by replacing them by nothing
+    t := bytes.ReplaceAll(p, []byte(" "), []byte(""))
+    //Some withe spaces can still be remaining
+    t = bytes.Trim(t, "\x00")
+    //Need to update the length of the byte
+    n, err = s.r.Read(t)
+	n = copy(p, t)
+	return n, err
+}
+func main() {
+    s := strings.NewReader("H e l l o w o r l d!")
+    r := spaceEraser{s}
+    io.Copy(os.Stdout, &r)
+}
+    
