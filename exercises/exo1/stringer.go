e1ff5770070284f55e258c51d5e072e43d26fcb7
olivierdg2 <16027@ecam.be>
2020-10-15 11:56:31 +0200

    Update stringer.go

--- exercises/exo1/stringer.go
+++ exercises/exo1/stringer.go
@@ -1,19 +1,42 @@
 package main
 
-import "fmt"
+import (
+    "fmt"
+    "math"
+)
 
-// Implement types Rectangle, Circle and Shape
-
+type Shape interface {
+    Area() float64
+    fmt.Stringer
+}
+type Circle struct {
+    r float64
+  }
+type Rectangle struct {
+    Width  float64
+    Length float64
+}
+func (r Rectangle) Area() float64{
+    return r.Width  * r.Length
+}
+func (c Circle) Area() float64{
+    return 3.14*math.Pow(c.r,2)
+}
+func (r Rectangle) String() string {
+    return fmt.Sprintf("Rectangle of width %f and length %f", r.Width, r.Length)
+}
+func (c Circle) String() string {
+    return fmt.Sprintf("Circle of radius %f", c.r)
+}
 func main() {
-	r := &Rectangle{2, 3}
-	c := &Circle{5}
+    r := &Rectangle{2, 3}
+    c := &Circle{5}
 
-	shapes := []Shape{r, c}
+    shapes := []Shape{r, c}
 
-	for _, s := range shapes {
-		fmt.Println(s)
-		// Expected output:
-		// Square of width 2 and length 3
-		// Circle of radius 5
-	}
+    for _, s := range shapes {
+        fmt.Println(s)
+        fmt.Println("And area:",s.Area())
+    }
+
 }
